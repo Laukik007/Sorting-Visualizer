d40189d81d3cee30c6fb330a1a5a042957179e96
Sankalp Srivastava <raunaksrivastava22@gmail.com>
2021-09-21 23:59:26 +0530

    added custom array button with validation for csv input`

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var container=document.getElementById("array");
 let x = false;
 let cusAry = 0;
 function StartStop()
@@ -18,6 +19,7 @@ function StartStop()
         document.getElementById("CusAry").disabled = false;
     }
 }
+<<<<<<< HEAD
 function customArray()
 {   if (cusAry ==0)
     {
@@ -33,5 +35,44 @@ function customArray()
         document.getElementById("inputArray").appendChild(txt); 
         document.getElementById("inputArray").appendChild(btn);
         cusAry =1;
+=======
+
+//to clear the previous randomly genrated array
+function clearBox(elementID) {
+    var div = document.getElementById(elementID);
+      
+    while(div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+//random genration function 
+function generatearray(){
+    //clearBox(array);
+    clearBox("array");
+    for(let i=0; i<20; i++){
+
+        //return random value in range 1 to 100
+        let value=Math.ceil(Math.random()*100);
+
+        //creating div element
+        var arr_ele=document.createElement("div");
+
+        //adding class to 'bars' to div
+        arr_ele.classList.add("bars");
+
+        //adding style to div
+        arr_ele.style.height=`${value*3}px`;
+        arr_ele.style.transform=`translate(${i*30}px)`;
+
+        //creating lable element for display
+        //size of a particular block
+        var arr_ele_lable=document.createElement("label");
+        arr_ele_lable.classList.add("bars_id");
+        arr_ele_lable.innerText=value;
+
+        arr_ele.appendChild(arr_ele_lable);
+        container.appendChild(arr_ele);
+>>>>>>> f24ec12c95ff6f19277a753cf4d4b6ede1f52be5
     }
 }
